86b9a3ece6724fe6ca37b03bd31de22a40e099ed
Rennie Bevineau <rsbevineau@gmail.com>
2020-09-27 21:16:03 -0400

    Updates

---
+++ precache-manifest.3854356dc5148e7618a6de883204dd9b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9654d682c399877f16700895e077469f",
+    "url": "/total_dance_virtual_classes/index.html"
+  },
+  {
+    "revision": "c0324bd7d9b5c993583e",
+    "url": "/total_dance_virtual_classes/static/css/main.76ba0926.chunk.css"
+  },
+  {
+    "revision": "cac90d225c96079d4465",
+    "url": "/total_dance_virtual_classes/static/js/2.54a313db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/total_dance_virtual_classes/static/js/2.54a313db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0324bd7d9b5c993583e",
+    "url": "/total_dance_virtual_classes/static/js/main.73a42b80.chunk.js"
+  },
+  {
+    "revision": "d638062c31c7103828d8",
+    "url": "/total_dance_virtual_classes/static/js/runtime-main.9c9a9861.js"
+  },
+  {
+    "revision": "d68940a427002a8d6e5668c8b3735290",
+    "url": "/total_dance_virtual_classes/static/media/bachata.d68940a4.png"
+  },
+  {
+    "revision": "26deda8bf525ccefb0019d81d7cecdfb",
+    "url": "/total_dance_virtual_classes/static/media/choose.26deda8b.png"
+  },
+  {
+    "revision": "84ddb725a0c044aee04dd2537733e9a8",
+    "url": "/total_dance_virtual_classes/static/media/eye_look.84ddb725.png"
+  },
+  {
+    "revision": "5547495176e776ba0899c2bb9d732f54",
+    "url": "/total_dance_virtual_classes/static/media/merengue.55474951.png"
+  },
+  {
+    "revision": "128702985d8f8aef4858be8d113886a5",
+    "url": "/total_dance_virtual_classes/static/media/salsa.12870298.png"
+  },
+  {
+    "revision": "9a6abf2d9efda28559551c33c3d12a59",
+    "url": "/total_dance_virtual_classes/static/media/user.9a6abf2d.png"
+  }
+]);
