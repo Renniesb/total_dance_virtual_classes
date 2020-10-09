eedff8bc97e4f1e8338fb923e29e6c82bb1025d1
Rennie Bevineau <rsbevineau@gmail.com>
2020-10-09 12:42:47 -0400

    update config to have all caps for endpoint environmental variable

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_Endpoint || 'http://localhost:8000/api',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
 }
