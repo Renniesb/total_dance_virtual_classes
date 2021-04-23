c1534da11a5c16fd5d0f947decb262968a6f2a04
Rennie Bevineau <rsbevineau@gmail.com>
2021-04-22 21:15:49 -0400

    get rid of svg spacing at the bottom

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -17,7 +17,7 @@ const Profile = ({getProfile,user_name,nickname,full_name, email,logout }) => {
                 <h3 style={{display: "inline"}}>Full Name:<span style={{color: "rebeccapurple"}}> {full_name}</span></h3>
                 <h3 style={{display: "inline"}}>Email:<span style={{color: "rebeccapurple"}}> {email}</span></h3>
             </div>
-                <svg style={{display: "block" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#333333" fillOpacity="1" d="M0,224L48,202.7C96,181,192,139,288,128C384,117,480,139,576,170.7C672,203,768,245,864,229.3C960,213,1056,139,1152,122.7C1248,107,1344,149,1392,170.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+                <svg style={{position: "absolute", bottom: 0, zIndex: "5" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#333333" fillOpacity="1" d="M0,224L48,202.7C96,181,192,139,288,128C384,117,480,139,576,170.7C672,203,768,245,864,229.3C960,213,1056,139,1152,122.7C1248,107,1344,149,1392,170.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
         </div>
         
         </>
